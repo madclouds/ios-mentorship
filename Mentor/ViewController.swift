5ed4a9f8a2758452a7e0ed546626e29cea82defe
Erik Bye <madclouds@gmail.com>
2024-03-25 09:20:13 -0700

    Initial Commit

---
+++ Mentor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mentor
+//
+//  Created by Erik Bye on 3/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
