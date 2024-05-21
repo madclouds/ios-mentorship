58b838d5f15fb7e2653faaa10cde29f1adfcaac4
Erik Bye <madclouds@gmail.com>
2024-05-21 09:55:18 -0700

    chore: remove example code

--- Mentor/ViewController.swift
+++ Mentor/ViewController.swift
@@ -8,45 +8,10 @@
 import UIKit
 import SnapKit
 
-protocol ViewControllerDeleage {
-    func handleButtonPress(_ sourceButton: UIButton)
-}
-
 class ViewController: UIViewController {
-    
-    var delegate: ViewControllerDeleage?
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .blue
-        
-        
-        let label = UILabel()
-        label.text = "HELLO WORLD"
-        label.textAlignment = .center
-        view.addSubview(label)
-        
-        label.snp.makeConstraints { make in
-            make.center.equalToSuperview()
-            make.height.equalTo(60)
-            make.width.equalTo(200)
-        }
-        
-        let button = UIButton()
-        button.setTitle("BUTTON", for: .normal)
-        button.setTitle("BUTTON Selected", for: .highlighted)
-        view.addSubview(button)
-        
-        button.snp.makeConstraints { make in
-            make.top.equalTo(label.snp.bottom).offset(20)
-            make.centerX.equalToSuperview()
-        }
-        button.addTarget(self, action: #selector(handleButtonAction(_:)), for: .touchUpInside)
-    }
-    
-    @objc func handleButtonAction(_ source: UIButton) {
-        delegate?.handleButtonPress(source)
     }
 }
 
